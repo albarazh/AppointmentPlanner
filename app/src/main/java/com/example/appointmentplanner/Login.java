2fd6e775e82e9f805cde55cde5e9f49111ecb5d5
hamza <hamza.albarazi.033@gmail.com>
2021-10-26 06:54:02 +0200

    update

--- app/src/main/java/com/example/appointmentplanner/Login.java
+++ app/src/main/java/com/example/appointmentplanner/Login.java
@@ -3,35 +3,34 @@ package com.example.appointmentplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
-        Button login = (Button) findViewById(R.id.loginButton);
-        Button register = (Button) findViewById(R.id.regButoon);
+        setContentView(R.layout.activity_login);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        Button back = (Button) findViewById(R.id.back);
+        Button cancel = (Button) findViewById(R.id.cancel);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                openpage(Login.class);
+                openpage(MainActivity.class);
             }
         });
-        register.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                openpage(Register.class);
+                openpage(MainActivity.class);
             }
         });
     }
-    public void openpage (Class classname){
+    public void openpage(Class classname){
         Intent intent = new Intent(this, classname);
         startActivity(intent);
     }
---
+++ app/src/main/java/com/example/appointmentplanner/Register.java
@@ -0,0 +1,34 @@
+package com.example.appointmentplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_register);
+        Button back = (Button) findViewById(R.id.back);
+        Button cancel = (Button) findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openMain(MainActivity.class);
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openMain(MainActivity.class);
+            }
+        });
+    }
+    public void openMain(Class classname){
+        Intent intent = new Intent(this, classname);
+        startActivity(intent);
+    }
+}

a74d57aa6223e7bcaaf62d1c81adbe2ae0d2f306
hamza <hamza.albarazi.033@gmail.com>
2021-11-17 20:46:47 +0100

    update

---
+++ app/src/main/java/com/example/appointmentplanner/To_Do.java
@@ -0,0 +1,14 @@
+package com.example.appointmentplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class To_Do extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+}
